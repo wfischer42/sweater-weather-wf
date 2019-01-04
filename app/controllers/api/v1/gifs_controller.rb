cc31e5d988ebf2ba8b28280139f8ef3cac91c94e
William Fischer <william.fischer42@gmail.com>
2019-01-04 10:19:31 -0700

    Added Giffy Service

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::GifsController < ApplicationController
   def show
     if location = Location.fetch(location_params)
       daily_forecast = Forecast.for_location(location).daily
-      forecast_gifs = Giforecast.for_forecast(daily_forecast)
+      forecast_gifs = Giforecast.for_forecast(daily_forecast, location)
       render json: GiforecastSerializer.new(forecast_gifs)
     else
       render json: { message: "Location was not found." }
--- app/models/giforecast.rb
+++ app/models/giforecast.rb
@@ -1,12 +1,13 @@
 class Giforecast
-  def initialize()
+  def initialize(raw_gifs)
 
   end
 
-  def self.for_forecast(daily_forecast)
+  def self.for_forecast(daily_forecast, location)
     raw_gifs = daily_forecast.map do |forecast, i|
       forecast[:url] = GiffyService.get_gif(forecast[:summary])
       forecast.slice(:time, :summary, :url)
     end
+    self.new(raw_gifs)
   end
 end
