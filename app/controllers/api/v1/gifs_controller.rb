2b27d04a99e32724fb6298cfb403f2c0d6e4d79d
William Fischer <william.fischer42@gmail.com>
2019-01-04 10:39:30 -0700

    Added Giforecast serializer. All Tests passing.

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::GifsController < ApplicationController
   def show
     if location = Location.fetch(location_params)
-      daily_forecast = Forecast.for_location(location).daily
-      forecast_gifs = Giforecast.for_forecast(daily_forecast, location)
+      daily_forecasts = Forecast.for_location(location).daily
+      forecast_gifs = Giforecast.for_forecast(daily_forecasts, location)
       render json: GiforecastSerializer.new(forecast_gifs)
     else
       render json: { message: "Location was not found." }
--- app/models/giforecast.rb
+++ app/models/giforecast.rb
@@ -1,13 +1,23 @@
-class Giforecast
-  def initialize(raw_gifs)
+class Giforecast < SimpleDelegator
+  attr_reader :daily_forecasts
 
+  def initialize(forecasts_gif_data, location)
+    @daily_forecasts = forecasts_gif_data
+    @location = location
   end
 
-  def self.for_forecast(daily_forecast, location)
-    raw_gifs = daily_forecast.map do |forecast, i|
+  def self.for_forecast(daily_forecasts_data, location)
+    forecasts_gif_data = daily_forecasts_data.map do |forecast, i|
       forecast[:url] = GiffyService.get_gif(forecast[:summary])
       forecast.slice(:time, :summary, :url)
     end
-    self.new(raw_gifs)
+    self.new(forecasts_gif_data, location)
   end
+
+  def id
+    location.id
+  end
+
+  private
+  attr_reader :location
 end
