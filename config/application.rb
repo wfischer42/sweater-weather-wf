0aedddc5119540b5c9475a5bc8395ca75d1e64c4
William Fischer <william.fischer42@gmail.com>
2019-01-24 13:32:13 -0700

    Configure CORS

--- config/application.rb
+++ config/application.rb
@@ -19,12 +19,6 @@ Bundler.require(*Rails.groups)
 
 module SweaterWeather
   class Application < Rails::Application
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins 'https://wfischer42.github.io/*'
-        resource '/api/*', headers: :any, methods: [:get, :post]
-      end
-    end
 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
