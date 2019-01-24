f30a9d04133f8a7fc7141e5489279053800c9a66
William Fischer <william.fischer42@gmail.com>
2019-01-24 13:05:06 -0700

    Configure CORS

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module SweaterWeather
   class Application < Rails::Application
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'localhost:8080', 'https://wfischer42.github.io/sweater-weather'
+        origins 'https://wfischer42.github.io'
         resource '*', headers: :any, methods: [:get, :post]
       end
     end
