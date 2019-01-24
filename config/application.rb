4f32e5e7421cbcc8fbecb6b729aa853b099ef946
William Fischer <william.fischer42@gmail.com>
2019-01-24 12:53:03 -0700

    Allow cross origin resource sharing

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,13 @@ Bundler.require(*Rails.groups)
 
 module SweaterWeather
   class Application < Rails::Application
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'localhost:8080', 'https://wfischer42.github.io/sweater-weather'
+        resource '*', headers: :any, methods: [:get, :post]
+      end
+    end
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
